40d4b3e15db0f586d89272ce6e3778f7cfa6dea6
Leissan Khan <96837622+Leissan@users.noreply.github.com>
2022-03-25 21:00:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(array, target){
+//Iterate through each number in the array
+for (let i = 0; i < array.length; i++) {
+  //n steps  for the array
+  //for the current numebr, identify complement that adds to the target (comp=target - current numebr)
+  const complement = target - array[i]
+  //iterate through the rest of the array
+  for (let j = i + 1; j < array.length; j++){
+    // loop inside the loop, so n * n steps
+   //check if any numebr is a compliment
+   //if so, return true
+    if (array[j] === complement) return true
+  }
 }
-
+//if i reach the end of the array, and no 2 numebrs addup to the target sum, return false
+return false
+}
 /* 
   Write the Big O time complexity of your function here
 */
